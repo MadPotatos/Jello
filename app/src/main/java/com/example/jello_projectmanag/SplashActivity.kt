f2c4afd4a30f1beacbb13c69238c385bec2bf362
VietNguyen <viet7896@gmail.com>
2023-04-14 17:01:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jello_projectmanag/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.jello_projectmanag
+
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.jello_projectmanag.databinding.ActivitySplashBinding
+
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.otf")
+        binding.tvSplashScreenTitle.typeface = typeFace
+    }
+}
