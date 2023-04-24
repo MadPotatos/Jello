96b4c7e62830381ea48799ea0175b02f1da9b50d
VietNguyen <viet7896@gmail.com>
2023-04-24 22:24:57 +0700

    [feature/login] Login user with email and password

--- app/src/main/java/com/example/jello_projectmanag/activities/SignInActivity.kt
+++ app/src/main/java/com/example/jello_projectmanag/activities/SignInActivity.kt
@@ -1,20 +1,30 @@
 package com.example.jello_projectmanag.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.jello_projectmanag.R
 import com.example.jello_projectmanag.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignInActivity : BaseActivity() {
+
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
         setupActionBar()
 
         // Hide the status bar
@@ -29,6 +39,7 @@ class SignInActivity : BaseActivity() {
         }
 
     }
+
     //Set up the action bar
     private fun setupActionBar() {
         setSupportActionBar(binding.toolbarSignInActivity)
@@ -38,5 +49,52 @@ class SignInActivity : BaseActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_24)
         }
         binding.toolbarSignInActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        binding.btnSignIn.setOnClickListener {
+            signInRegisteredUser()
+        }
+    }
+
+    //Sign in using email and password
+    private fun signInRegisteredUser() {
+        // Get the text from editText and trim the space
+        val email: String = binding.etEmailSignin.text.toString().trim { it <= ' ' }
+        val password: String = binding.etPasswordSignin.text.toString()
+        if (validateForm(email, password)) {
+            showProgressDialog(resources.getString(R.string.please_wait))
+            // Sign in using FirebaseAuth
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        // Sign in success
+                        val user = auth.currentUser
+                        startActivity(Intent(this, MainActivity::class.java))
+
+                    } else {
+                        // Sign in failed
+                        Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+
+    // Validate
+    private fun validateForm(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email))
+                false
+            }
+
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password))
+                false
+            }
+
+            else -> {
+                true
+            }
+        }
     }
 }
