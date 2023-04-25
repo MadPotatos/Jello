e490b08497f986ece0f52ca222d9267b8fd106e6
VietNguyen <viet7896@gmail.com>
2023-04-26 00:34:05 +0700

    [feature/login] Create User model

---
+++ app/src/main/java/com/example/jello_projectmanag/firebase/FirestoreClass.kt
@@ -0,0 +1,12 @@
+package com.example.jello_projectmanag.firebase
+
+import com.example.jello_projectmanag.activities.SignUpActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreClass {
+
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+
+
+}
---
+++ app/src/main/java/com/example/jello_projectmanag/models/User.kt
@@ -0,0 +1,15 @@
+package com.example.jello_projectmanag.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    var id: String = "",
+    var name: String = "",
+    var email: String = "",
+    var image: String = "",
+    var mobile: Long = 0,
+    var fcmToken: String = ""
+
+): Parcelable
