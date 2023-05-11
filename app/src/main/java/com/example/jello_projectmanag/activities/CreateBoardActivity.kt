8becc1d425743a942f0c42fe4aacf58e0838cdf1
VietNguyen <viet7896@gmail.com>
2023-05-11 22:54:31 +0700

    [feature/create_board] Image chooser for board

--- app/src/main/java/com/example/jello_projectmanag/activities/CreateBoardActivity.kt
+++ app/src/main/java/com/example/jello_projectmanag/activities/CreateBoardActivity.kt
@@ -1,14 +1,40 @@
 package com.example.jello_projectmanag.activities
 
+import android.app.Activity
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.example.jello_projectmanag.R
 import com.example.jello_projectmanag.databinding.ActivityCreateBoardBinding
+import com.example.jello_projectmanag.utils.Constants
 
 
 class CreateBoardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCreateBoardBinding
+    private var mSelectedImageFileUri: Uri? = null
+
+    val getBoardImage = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) {
+        if(it.resultCode == Activity.RESULT_OK && it.data != null){
+            mSelectedImageFileUri = it.data!!.data
+            try {
+                Glide
+                    .with(this)
+                    .load(mSelectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_board_place_holder)
+                    .into(binding.ivBoardImage)
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateBoardBinding.inflate(layoutInflater)
@@ -16,6 +42,18 @@ class CreateBoardActivity : AppCompatActivity() {
         setContentView(view)
 
         setupActionBar()
+
+        binding.ivBoardImage.setOnClickListener{
+            // If the permission is already allowed or we need to request for it.
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                == android.content.pm.PackageManager.PERMISSION_GRANTED){
+                Constants.showImageChooser(this)
+            }
+            else{
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.READ_STORAGE_PERMISSION_CODE)
+            }
+        }
     }
     private fun setupActionBar() {
         setSupportActionBar(binding.toolbarCreateBoardActivity)
@@ -28,4 +66,22 @@ class CreateBoardActivity : AppCompatActivity() {
         binding.toolbarCreateBoardActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
 
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if( requestCode == Constants.READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty()
+                && grantResults[0] == android.content.pm.PackageManager.PERMISSION_GRANTED){
+                Constants.showImageChooser(this)
+            }
+        }else{
+            Toast.makeText(this, "Oops, you just denied the permission for storage. You can allow it from the settings.",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
--- app/src/main/java/com/example/jello_projectmanag/utils/Constants.kt
+++ app/src/main/java/com/example/jello_projectmanag/utils/Constants.kt
@@ -1,5 +1,13 @@
 package com.example.jello_projectmanag.utils
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import android.webkit.MimeTypeMap
+import com.example.jello_projectmanag.activities.CreateBoardActivity
+import com.example.jello_projectmanag.activities.MyProfileActivity
+
 object Constants {
 
     const val USERS: String = "users"
@@ -7,4 +15,25 @@ object Constants {
     const val IMAGE: String = "image"
     const val NAME: String = "name"
     const val MOBILE: String = "mobile"
+    const val READ_STORAGE_PERMISSION_CODE = 1
+
+    fun showImageChooser(activity: Activity) {
+        // An intent for launching the image selection of phone storage.
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        // Launches the image selection of phone storage using the constant code.
+        when (activity) {
+            is MyProfileActivity -> {
+                activity.getProfileImage.launch(galleryIntent)
+            }
+            is CreateBoardActivity -> {
+                activity.getBoardImage.launch(galleryIntent)
+            }
+        }
+    }
+
+    //  Get the file extension of a file referenced by a Uri
+    fun getFileExtension(activity: Activity, uri: Uri?): String? {
+        return MimeTypeMap.getSingleton()
+            .getExtensionFromMimeType(activity.contentResolver.getType(uri!!))
+    }
 }
