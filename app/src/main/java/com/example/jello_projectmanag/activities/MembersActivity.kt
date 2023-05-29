9e92d0625c74dda624e59178b76c21fac92b531b
VietNguyen <viet7896@gmail.com>
2023-05-29 22:53:25 +0700

    [feature/task_list] Setup Member Item

--- app/src/main/java/com/example/jello_projectmanag/activities/MembersActivity.kt
+++ app/src/main/java/com/example/jello_projectmanag/activities/MembersActivity.kt
@@ -1,12 +1,44 @@
 package com.example.jello_projectmanag.activities
 
+import android.os.Build
+import android.os.Build.VERSION
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jello_projectmanag.R
+import com.example.jello_projectmanag.databinding.ActivityMembersBinding
+import com.example.jello_projectmanag.models.Board
+import com.example.jello_projectmanag.utils.Constants
 
 class MembersActivity : AppCompatActivity() {
+
+    private lateinit var mBoardDetails: Board
+    private lateinit var binding: ActivityMembersBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_members)
+        binding = ActivityMembersBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        if(intent.hasExtra(Constants.BOARD_DETAIL)){
+            mBoardDetails = if(VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                intent.getParcelableExtra(Constants.BOARD_DETAIL, Board::class.java)!!
+
+            } else{
+                @Suppress("DEPRECATION")
+                intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+            }
+        }
+
+        setupActionBar()
+    }
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarMembersActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = resources.getString(R.string.members)
+        }
+        binding.toolbarMembersActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
 }
